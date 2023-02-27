77ef70328965bf3be5330f1a2ff3613013e802f2
liaYaghobi <liayaghobi@gmail.com>
2023-02-27 15:46:34 -0800

    Creates Navbar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from '../Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       
+      <Navbar />
+     
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+function Navbar(){
+
+
+    return (
+        <div className='Navbar'>
+           
+        </div>
+    )
+}
+
+export default Navbar
