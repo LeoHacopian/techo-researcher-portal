f8977b7bbbe6e30541ffede3ba937d826246ad6b
LeoHacopian <65243277+LeoHacopian@users.noreply.github.com>
2023-02-27 16:18:43 -0800

    Updates Navbar by adding MUI and state variables

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
 import './Navbar.css'
+import { Button, Avatar } from '@mui/material'
+import { useState } from 'react'
 
 function Navbar(){
-
-
+    const [number, setNumber] = useState(0)
+    function increment(){
+        setNumber(number + 1)
+    }
     return (
         <div className='Navbar'>
-           
+            <Button variant='contained' onClick={increment}>Hello</Button>
+            <div>{number}</div>
+            {/* <Avatar>LH</Avatar> */}
         </div>
     )
 }
